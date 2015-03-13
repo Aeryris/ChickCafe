2d1ddf2427e94b5f955ffd7d90db581c91dcddaa
Bartek Kliszczyk <aeryris@gmail.com>
2015-03-13 14:49:04 +0000

    ..

--- system/Auth.php
+++ system/Auth.php
@@ -36,7 +36,7 @@
 class Auth extends Acl{
 
     public function isAuth(){
-
+        //if($_SESSION['a'] == $_SESSION['key'] && )
     }
 
     public function login($aParams){
@@ -45,6 +45,48 @@ class Auth extends Acl{
 
     public function register($aParams){
 
+    }
+
+    /**
+     * Generates a secure, pseudo-random password with a safe fallback.
+     */
+    function pseudo_rand($length) {
+        if (function_exists('openssl_random_pseudo_bytes')) {
+            $is_strong = false;
+            $rand = openssl_random_pseudo_bytes($length, $is_strong);
+            if ($is_strong === true) return $rand;
+        }
+        $rand = '';
+        $sha = '';
+        for ($i = 0; $i < $length; $i++) {
+            $sha = hash('sha256', $sha . mt_rand());
+            $chr = mt_rand(0, 62);
+            $rand .= chr(hexdec($sha[$chr] . $sha[$chr + 1]));
+        }
+        return $rand;
+    }
+
+    /**
+     * Creates a very secure hash. Uses blowfish by default with a fallback on SHA512.
+     */
+    function create_hash($string, &$salt = '', $stretch_cost = 10) {
+        $salt = pseudo_rand(128);
+        $salt = substr(str_replace('+', '.', base64_encode($salt)), 0, 22);
+        if (function_exists('hash') && in_array($hash_method, hash_algos()) {
+		return crypt($string, '$2a$' . $stretch_cost . '$' . $salt);
+    }
+        return _create_hash($string, $salt);
+    }
+
+    /**
+     * Fall-back SHA512 hashing algorithm with stretching.
+     */
+    function _create_hash($password, $salt) {
+        $hash = '';
+        for ($i = 0; $i < 20000; $i++) {
+            $hash = hash('sha512', $hash . $salt . $password);
+        }
+        return $hash;
     }
 
 
