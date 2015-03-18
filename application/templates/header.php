1d4384cf527509b5e901a46f7f47a516d2797d2a
Gideon <gideonb234@gmail.com>
2015-03-18 13:39:44 +0000

    Added in login/register
    Functionality not added yet, converting to MVC model

--- application/templates/header.php
+++ application/templates/header.php
@@ -9,17 +9,36 @@
     <title>ChickCafe</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <link rel="stylesheet" href="/application/assets/css/bootstrap.min.css">
-    <style>
-        body {
-            padding-top: 50px;
-            padding-bottom: 20px;
-        }
-    </style>
     <link rel="stylesheet" href="/application/assets/css/bootstrap-theme.min.css">
-
-
+    <link rel="stylesheet" href="/application/assets/css/style.css">
     <script src="/application/assets/js/modernizr.min.js"></script>
 </head>
 <body>
+<nav class="navbar navbar-default">
+<div class="container-fluid">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#">
+      <a href="/index.php"><img alt="Chick Cafe" src="..."></a>
+    </a>
+  </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse" id="nav-options">
+    <ul class="nav navbar-nav navbar-right">
+        <!-- If logged in -->
+        <li><a href="#"><i class="fa fa-shopping-cart"></i> Basket</a></li>
+        <!-- If not logged in -->
+        <li><a href="#">Sign-in</a></li>
+        <li><a href="#">Register</a></li>
+    </ul>
+  </div><!-- /.navbar-collapse -->
+</div><!-- /.container-fluid -->
+</nav>
--- application/templates/index.php
+++ application/templates/index.php
@@ -1,17 +1,21 @@
 {include file=header.php}
 
-
 {! $test }
 
-<form action="/user/login" method="post">
+<div id="view_menu">
+	<div class="btn-group" role="group" aria-label="...">
+		<button type="button" class="btn btn-default btn-lg">Breakfast</button>
+		<button type="button" class="btn btn-default btn-lg">Lunch</button>
+		<button type="button" class="btn btn-default btn-lg">Dinner</button>
+		<button type="button" class="btn btn-default btn-lg">Drinks</button>
+	</div>
+	<button type="button" class="btn btn-default btn-lg navbar-btn select">Select</button>
+</div>
 
-    <label for="username">Username</label>
-    <input type="text" name="username" />
-
-    <label for="password">Password</label>
-    <input type="password" name="password" />
-
-    <button type="submit" value="Submit">Submit</button>
-
-</form>
-
+<div id="daily_special">
+	<div class="jumbotron">
+		<h1>Today's Daily Special</h1>
+		<p>Today's daily special is...</p>
+		<img src="" alt="Daily Special is">
+	</div>
+</div>
